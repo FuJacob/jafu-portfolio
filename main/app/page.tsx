f535e6b9e833d25aba5bb3142c0b74837b231234
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2025-12-27 00:31:34 -0500

    fix: chatbox width

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
           </div>
 
           {/* Mini chat input below content box */}
-          <div className="w-full px-2 md:px-4 mt-1 md:mt-2">
+          <div className="w-full mt-1 md:mt-2">
             <MiniChatInput
               value={inputValue}
               onChange={setInputValue}
